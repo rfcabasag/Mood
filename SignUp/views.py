1805513d7170b6a44ba01ee02c96892cb615895e
jmIbardaloza <34402819+jmIbardaloza@users.noreply.github.com>
2019-02-08 14:23:14 +0800

    added license

--- SignUp/views.py
+++ SignUp/views.py
@@ -1,3 +1,18 @@
+"""
+
+This is a course requirement for CS 192 Software Engineering II under the supervision of Asst. Prof. Ma. Rowena C. Solamo of the Department of Computer Science, College of Engineering, University of the Philippines, Diliman for the AY 2018-2019.
+	Author: Jose Maria C. Ibardaloza
+	Code History:
+		Programmer				        Change Date			Change Description
+		Jose Maria C. Ibardaloza		02/05/19		    added the SignUp class
+	Creation Date: 02/05/2019
+	Development Group: Team consisting of Robert Cabasag, Jose Maria Ibardaloza, and Katreen Hernandez
+	Client Group: Students meaning to have a sense of mindfulness
+	
+	Creates the Sign up view that the app will use.
+
+"""
+
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
