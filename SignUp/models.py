3d9f26222022835330c199f1646c6516c0810ec4
rfcabasag <rfcabasag@up.edu.ph>
2019-02-20 17:08:56 +0800

    single add mood scenario

--- SignUp/models.py
+++ SignUp/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
+"""
+This is a course requirement for CS 192 Software Engineering II under the supervision of Asst. Prof. Ma. Rowena C. Solamo of the Department of Computer Science, College of Engineering, University of the Philippines, Diliman for the AY 2018-2019.
+	Author: Jose Maria C. Ibardaloza
+	Code History:
+		Programmer				        Change Date			Change Description
+		Robert Daniel F Cabasag		02/19/19			added the RegisteredUser model
+	Creation Date: 02/06/2019
+	Development Group: Team consisting of Robert Cabasag, Jose Maria Ibardaloza, and Katreen Hernandez
+	Client Group: Students meaning to have a sense of mindfulness
+	
+	The differents objects and schema of the objects are shown in this file.
+"""
 
-# Create your models here.
+# this is the data model for the registered user
+# it only records the username
+class RegisteredUser(models.Model):
+	user = models.OneToOneField(User, on_delete=models.CASCADE)
+	def __str__(self):
+		return self.user.username
--- maintainmood/models.py
+++ maintainmood/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
+from SignUp.models import RegisteredUser
 
+class Mood(models.Model):
+	id = models.AutoField(primary_key=True)
+	user = models.ForeignKey(RegisteredUser,blank=False,on_delete=models.CASCADE)
+	mood = models.TextField(blank=True)
+	def __str__(self):
+		return self.mood
