3ade241ef60ca75d6caf1c5602eb8ef24b642050
jmIbardaloza <34402819+jmIbardaloza@users.noreply.github.com>
2019-02-21 16:45:46 +0800

    Update urls.py
    added delete mood path

--- maintainmood/urls.py
+++ maintainmood/urls.py
@@ -5,6 +5,7 @@ This is a course requirement for CS 192 Software Engineering II under the superv
 	Code History:
 		Programmer				        Change Date			Change Description
 		Robert Daniel Cabasag			02/17/19		    added the addmood path
+		Jose Maria C. Ibardaloza		02/21/19		    added deletemood path
 	Creation Date: 02/17/2019
 	Development Group: Team consisting of Robert Cabasag, Jose Maria Ibardaloza, and Katreen Hernandez
 	Client Group: Students meaning to have a sense of mindfulness
@@ -20,4 +21,5 @@ urlpatterns = [
 	path('', views.moodpage, name='moodpage'),
 	path('addmood/', views.addmood, name='addmood'),
 	path('addstatus/', views.addstatus, name='addstatus'),
+	path('<int:mood_id>/delete', views.deletemood, name='deletemood'),
 ]
