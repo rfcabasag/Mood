90b1fe1bb9d8a0547fbaf6b2e79f21e2166be3fb
rfcabasag <rfcabasag@up.edu.ph>
2019-02-22 13:31:23 +0800

    Update models.py

--- maintainmood/models.py
+++ maintainmood/models.py
@@ -1,17 +1,3 @@
-"""
-
-This is a course requirement for CS 192 Software Engineering II under the supervision of Asst. Prof. Ma. Rowena C. Solamo of the Department of Computer Science, College of Engineering,  University of the Philippines, Diliman for the AY 2018-2019.
-	Author: Robert Daniel Cabasag
-	Code History:
-		Programmer				        Change Date			Change Description
-		Robert Daniel Cabasag			02/17/19		    created the mood model
-	Creation Date: 02/17/2019
-	Development Group: Team consisting of Robert Cabasag, Jose Maria Ibardaloza, and Katreen Hernandez
-	Client Group: Students meaning to have a sense of mindfulness
-	
-	This file contains the object models for the maintainmood app.
-"""
-
 from django.db import models
 from django.contrib.auth.models import User
 from SignUp.models import RegisteredUser
