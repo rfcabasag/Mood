c449af81b709bd8543e05a2bd6374f6a8a11c6c4
jmIbardaloza <34402819+jmIbardaloza@users.noreply.github.com>
2019-03-07 23:11:15 +0800

    Updated urls.py
    added delete activity path

--- maintainactivity/urls.py
+++ maintainactivity/urls.py
@@ -5,7 +5,8 @@ This is a course requirement for CS 192 Software Engineering II under the superv
 	Code History:
 		Programmer				        Change Date			Change Description
 		Robert Daniel Cabasag			03/05/19		    created file
-		Robert Daniel Cabasag			03/05/19		    added activitypage and addactivity paths			
+		Robert Daniel Cabasag			03/05/19		    added activitypage and addactivity paths
+		Jose Maria Ibardaloza			03/07/19		    added delete activity path
 	Creation Date: 03/05/2019
 	Development Group: Team consisting of Robert Cabasag, Jose Maria Ibardaloza, and Katreen Hernandez
 	Client Group: Students meaning to have a sense of mindfulness
@@ -20,5 +21,5 @@ from . import views
 urlpatterns = [
 	path('', views.activitypage, name='activitypage'),
 	path('addactivity/', views.addactivity, name='addactivity'),
-
+	path('<int:activity_id>/delete', views.deleteactivity, name='deleteactivity'),
 ]
